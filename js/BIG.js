4bbb0ff484259ef1a8411b8817513940cd542510
Ellen B Headrick <EBHeadrick@hotmail.com>
2014-08-30 13:35:49 -0600

    More changes trying different styles.

--- js/BIG.js
+++ js/BIG.js
@@ -2,18 +2,11 @@
  * Created by EB on 8/6/2014.
  */
 
-// You need this code for each tab in the page
 
 $(function() {
 
-    function navSetup() {
-        tabSetup(home);
-    }
+    $('#bigLogo').on('click', function(){
+        $('#homePill').addClass('active');
+    });
 
-    function tabSetup(tabName){
-        $(tabName+' a').click(function (e) {
-            e.preventDefault()
-            $(this).tab('show')
-        })
-    }
-} // end of function for the document ready
+}) // end of function for the document ready
